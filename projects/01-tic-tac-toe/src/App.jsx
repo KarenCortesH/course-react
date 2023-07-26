51ddb52648a7be63764e50e3f3e93ec88731c63a
Karen Cortes <karencortes9828@gmail.com>
2023-07-26 01:52:34 -0500

    feat: Update the state board and setTurn

--- projects/01-tic-tac-toe/src/App.jsx
+++ projects/01-tic-tac-toe/src/App.jsx
@@ -25,19 +25,23 @@ const Square = ({ children, isSelected, updateBoard, index }) => {
 }
 
 
-
 function App() {
   //tablero
   const [board, setBoard] = useState(Array(9).fill(null));
+  //console.log(board);
 
   //Saber el turno de quien
   const [turn, setTurn] = useState(Turns.X)
 
 
-  //va actuualizar el estado yy pintarlo en el tablero
+  //va actualizar el estado y pintar en el tablero una X o O
   const updateBoard = (index) => {
+    const newBoard = [...board]
+    newBoard[index] = turn
+    setBoard(newBoard);
+
     //Cambiar de turno de X a O
-    const newTurn = turn === Turns.X ? Turns.O : Turns.X
+    const newTurn = turn == Turns.X ? Turns.O : Turns.X
     setTurn(newTurn);
 
   }
@@ -60,10 +64,10 @@ function App() {
         }
       </section>
       <section className='turn'>
-        <Square isSelected={turn === Turns.X}>
+        <Square isSelected={turn == Turns.X}>
           {Turns.X}
         </Square>
-        <Square isSelected={turn === Turns.X}>
+        <Square isSelected={turn == Turns.O}>
           {Turns.O}
         </Square>
       </section>
