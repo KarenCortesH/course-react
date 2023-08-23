273e28dc74899af6c5adb2d9d7391a7b9b1e0fab
Karen Cortes <karencortes9828@gmail.com>
2023-08-23 17:14:45 -0500

    feat: update useState and handleSubmit

--- projects/04-react-search-films/src/App.jsx
+++ projects/04-react-search-films/src/App.jsx
@@ -1,29 +1,49 @@
 import './App.css'
 import { useMovies } from './hooks/useMovies'
 import { Movies } from './components/Movies'
-import { useRef } from 'react'
+import { useEffect, useRef, useState } from 'react'
 
 
 
 function App() {
   const { movies } = useMovies()
-  const inputRef = useRef()
+  //Creamos los estados para ver lo que esta pintando de forma controlada
+  const [query, setQuery] = useState('')
+  const [error, setError] = useState('')
 
   //Cuando hagamos clic en el buscador
-  const handleSubmit = (event) =>{
+  const handleSubmit = (event) => {
     event.preventDefault()
-  const inputElement = inputRef.current
-  //Aqui estamos recuperando el valor del elemento
-  const value = inputElement.value
-  console.log(value)
+    console.log( { query })
   }
+ //Cuando el estado se actualiza automaticamente lo vamos a ver en el input
+  const handleChange = (event) => {
+    setQuery(event.target.value)
+
+  }
+
+  //
+  useEffect(() =>{
+    if(query === '') {
+      setError ("No se puede buscar una pelicula vacia")
+      return
+    }
+    if(query.match (/^\d+$/))
+    {
+      setError("No se puede buscar una pelicula con numero")
+      return
+    }
+    setError(null)
+  }, [query])
   return (
     <div className='page'>
       <header>
         <h1>Buscador de Peliculas</h1>
         <form className="form" onSubmit={handleSubmit} >
-          <input ref={inputRef} placeholder='Avengers, Star Wars, The Matrix' />
+          <input name="query" value={query} onChange={handleChange} placeholder='Avengers, Star Wars, The Matrix' />
           <button type='submit'>Buscar</button>
+          {/* Aqui ponemos el error */}
+          {error && <p style={{color:'red'}}>{error}</p>}
         </form>
       </header>
 
