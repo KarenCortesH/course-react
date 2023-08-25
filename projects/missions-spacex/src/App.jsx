900dec29e99fff29ae4475f7878fe727aa55b819
Karen Cortes <karencortes9828@gmail.com>
2023-08-25 17:41:48 -0500

    feat: painting api with flight_number, release.mission_name, release.release year

--- projects/missions-spacex/src/App.jsx
+++ projects/missions-spacex/src/App.jsx
@@ -1,9 +1,27 @@
-function App() {
+import { useState, useEffect } from 'react'
+import * as API from './services/launches'
+export function App() {
+  //Vamos a guardar el array con los datos
+  const [launches, setLaunches] = useState([]);
+
+  useEffect(() => {
+    //Cuando acabe la promesa vamos a obtener los datos
+    API.getAllLaunches().then(setLaunches);
+  },[]);
+
   return (
     <>
-      <div>Hola MUNDO</div>
+    <h1>SpaceX Launches</h1>
+      <section>
+        {launches.map(launch =>(
+          <li  key={launch.flight_number}>
+            {launch.mission_name}({launch.launch_year})
+          </li>
+        ))}
+      </section>
     </>
   )
 }
+
 
 export default App
--- projects/missions-spacex/src/services/launches.js
+++ projects/missions-spacex/src/services/launches.js
@@ -1,8 +1,26 @@
-//Creare funcion que devuelva los datos de la url
+//Funcion que devuelva los datos del JSON
 const API_URL = "https://api.spacexdata.com/v3";
 
-export async function getAllLaunches(){
-    return fetch(`${API_URL}/launches`).then(response => {
-        return response.json();
-    })
+
+export async function getAllLaunches() {
+    try {
+        const response = await fetch(`${API_URL}/launches`);
+        //Aqui esta la respuesta en el json
+        const data = await response.json();
+        return data;
+
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+export async function getLaunchByFlightNumber(flightNumber){
+    try {
+        const response = await fetch(`${API_URL}/launches/${flightNumber}`);
+        //Aqui esta la respuesta en el json
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error(error)
+    }
 }
