c8d0ab39452a24b1567e79873d292db5b5530826
Karen Cortes <karencortes9828@gmail.com>
2023-08-29 13:48:54 -0500

    feat: implementation Chakra

--- projects/missions-spacex/src/App.jsx
+++ projects/missions-spacex/src/App.jsx
@@ -1,5 +1,9 @@
 import { useState, useEffect } from 'react'
+//_Importamos el heading de Chakra
+import { Box, Heading, Image } from '@chakra-ui/react'
 import * as API from './services/launches'
+//aqui importamos el logo
+import logo from './assets/logo-spacex.png'
 export function App() {
   //Vamos a guardar el array con los datos
   const [launches, setLaunches] = useState([]);
@@ -7,16 +11,18 @@ export function App() {
   useEffect(() => {
     //Cuando acabe la promesa vamos a obtener los datos
     API.getAllLaunches().then(setLaunches);
-  },[]);
+  }, []);
 
   return (
     <>
-    <h1>SpaceX Launches</h1>
+      <Image margin={4} src={logo} width={300}></Image>
+      <Heading as="h1" size="lg" margin={4}>SpaceX Launches</Heading>
       <section>
-        {launches.map(launch =>(
-          <li  key={launch.flight_number}>
+        {launches.map(launch => (
+          <Box key={launch.flight_number} bg="gray.100" p={4} m={4} borderRadius="lg">
             {launch.mission_name}({launch.launch_year})
-          </li>
+          </Box>
+
         ))}
       </section>
     </>
